d52f81e202b2f33e4e348aecfdfffa6d5a6ef227
Balazs Gyurak <ba32107@gmail.com>
2020-07-10 20:54:05 +0100

    Another test

--- Chrome.NativeMessaging/Chrome.NativeMessaging/INativeMessagingHost.cs
+++ Chrome.NativeMessaging/Chrome.NativeMessaging/INativeMessagingHost.cs
@@ -5,7 +5,7 @@ namespace io.github.ba32107.Chrome.NativeMessaging
 {
     /// <summary>
     /// Represents the native messaging host. Can be used to send a single message to Chrome, or to listen and
-    /// reply to messages continuously.f
+    /// reply to messages continuously.fdfd
     /// </summary>
     public interface INativeMessagingHost
     {
